a2381a03814f1a0698fcf30f52d1558046c1100a
csw823197541 <cx123456>
2016-05-30 09:04:00 +0800

    Initial commit

---
+++ src/generateResult/GenerateMoveInfoResult.java
@@ -0,0 +1,88 @@
+package generateResult;
+
+import importDataInfo.AutoStowResultInfo;
+import importDataInfo.CwpResultInfo;
+import importDataInfo.MoveInfo;
+import importDataProcess.ImportData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by leko on 2016/1/22.
+ */
+public class GenerateMoveInfoResult {
+
+    public static List<MoveInfo> getMoveInfoResult(List<CwpResultInfo> cwpResultInfoList, List<AutoStowResultInfo> autoStowResultInfoList){
+        List<MoveInfo> moveInfoList = new ArrayList<MoveInfo>();
+
+        Map<String,Integer> crane = new HashMap<>();     //桥机的moveID
+        Map<String,List<String>> moveOrderRecords = ImportData.moveOrderRecords;   //根据舱和moveorder确定具体位置
+        Map<String,String[]> autoStowResult = ImportData.autoStowResult;        //自动配载结果
+        List<CwpResultInfo> cwpResultInfoList1 = cwpResultInfoList;            //cwp结果
+
+
+        for (CwpResultInfo cwpResultInfo: cwpResultInfoList1) {
+            try {
+                String craneID = cwpResultInfo.getCRANEID();                //桥机号
+                String hatchID = cwpResultInfo.getHATCHID();                //舱号
+                String hatchBwId = cwpResultInfo.getHATCHBWID();//倍位号
+                Integer startMoveOrder = cwpResultInfo.getStartMoveID();        //舱内开始的moveorder
+                Integer endMoveOrder = cwpResultInfo.getEndMoveID();          //舱内结束的moveorder
+                Integer moveCount = cwpResultInfo.getMOVECOUNT();
+                Integer startTime = cwpResultInfo.getWORKINGSTARTTIME();
+                Integer endTime = cwpResultInfo.getWORKINGENDTIME();
+                String LD = cwpResultInfo.getLDULD();
+                String moveType = cwpResultInfo.getMOVETYPE();
+                Integer singleTime = (endTime-startTime)/moveCount;
+                for (int i = startMoveOrder; i < startMoveOrder + moveCount; i++) {
+                    String hatchMoveOrder = hatchID + "." + String.valueOf(i) + "." + moveType;          //舱号连接编号
+                    List<String> vesselPosition = moveOrderRecords.get(hatchMoveOrder);
+                    if(vesselPosition != null) {
+                        for(String str : vesselPosition) {
+                            MoveInfo moveInfo = new MoveInfo();
+                            moveInfo.setBatchId(craneID);               //批号为桥机号
+                            moveInfo.setMoveKind(LD);
+                            String vp = str.split("\\.")[1] + "" + str.split("\\.")[2] + "" + str.split("\\.")[3];
+                            moveInfo.setVesselPosition(vp);
+                            if (!crane.containsKey(craneID))
+                                crane.put(craneID,0);
+                            Integer moveID = crane.get(craneID) + i - startMoveOrder + 1;     //桥机的move序列
+                            moveInfo.setMoveId(moveID);
+                            moveInfo.setMoveType(moveType);
+                            moveInfo.setGkey(craneID + "@" + moveID.toString());
+//                            moveInfo.setWORKINGSTARTTIME(startTime + singleTime*(i - startMoveOrder));
+                            moveInfo.setWORKINGSTARTTIME(startTime);
+                            moveInfo.setWORKINGENDTIME(endTime);
+                            moveInfo.setWorkingStartTime(cwpResultInfo.getWorkingStartTime());
+                            moveInfo.setWorkingEndTime(cwpResultInfo.getWorkingEndTime());
+                            if(LD.equals("L")) {
+                                moveInfo.setExToPosition(str);
+                                String areaPosition = autoStowResult.get(str)[0];
+                                String unitID = autoStowResult.get(str)[1];
+                                String size = autoStowResult.get(str)[2];
+                                moveInfo.setExFromPosition(areaPosition);
+                                moveInfo.setUnitId(unitID);
+                                moveInfo.setUnitLength(size);
+                            } else {
+                                moveInfo.setExToPosition(str);
+                                String size = autoStowResult.get(str)[2];
+                                moveInfo.setExFromPosition(" ");
+                                moveInfo.setUnitId(" ");
+                                moveInfo.setUnitLength(size);
+                            }
+                            moveInfoList.add(moveInfo);
+                        }
+                    }
+                }
+                crane.put(craneID, crane.get(craneID) + moveCount);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        return moveInfoList;
+    }
+}
---
+++ src/importDataInfo/MoveInfo.java
@@ -0,0 +1,151 @@
+package importDataInfo;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by leko on 2016/1/22.
+ */
+public class MoveInfo {
+    private String gkey;                    //唯一编号
+    private Integer WORKINGSTARTTIME;     //开始时间
+    private Integer WORKINGENDTIME;         //结束时间
+    private String batchId;                 //桥机号
+    private int moveId;                     //桥机作业顺序moveId
+    private String moveKind;                //装卸标志
+    private String unitId;                  //箱编号
+    private String unitLength;              //箱尺寸
+    private String exFromPosition;      //计划提箱位置
+    private String exToPosition;       //计划放箱位置
+
+    private String vesselPosition;   //船箱位：倍.层.排
+    private Date workingStartTime;    //开始时间
+    private Date workingEndTime;    //结束时间
+
+    private String moveType;    //作业工艺
+
+    public String getMoveType() {
+        return moveType;
+    }
+
+    public void setMoveType(String moveType) {
+        this.moveType = moveType;
+    }
+
+    public String getVesselPosition() {
+        return vesselPosition;
+    }
+
+    public void setVesselPosition(String vesselPosition) {
+        this.vesselPosition = vesselPosition;
+    }
+
+    public Date getWorkingStartTime() {
+        return workingStartTime;
+    }
+
+    public void setWorkingStartTime(Date workingStartTime) {
+        this.workingStartTime = workingStartTime;
+    }
+
+    public Date getWorkingEndTime() {
+        return workingEndTime;
+    }
+
+    public void setWorkingEndTime(Date workingEndTime) {
+        this.workingEndTime = workingEndTime;
+    }
+
+    //获取属性列表
+    static public List getFiledsInfo() {
+        Field[] fields = MoveInfo.class.getDeclaredFields();
+        String[] fieldNames = new String[fields.length];
+        List list = new ArrayList();
+        for (int i = 0; i < fields.length; i++) {
+            list.add(fields[i].getName());
+        }
+        return list;
+    }
+
+    public String getGkey() {
+        return gkey;
+    }
+
+    public void setGkey(String gkey) {
+        this.gkey = gkey;
+    }
+
+    public Integer getWORKINGSTARTTIME() {
+        return WORKINGSTARTTIME;
+    }
+
+    public void setWORKINGSTARTTIME(Integer WORKINGSTARTTIME) {
+        this.WORKINGSTARTTIME = WORKINGSTARTTIME;
+    }
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public int getMoveId() {
+        return moveId;
+    }
+
+    public void setMoveId(int moveId) {
+        this.moveId = moveId;
+    }
+
+    public String getMoveKind() {
+        return moveKind;
+    }
+
+    public void setMoveKind(String moveKind) {
+        this.moveKind = moveKind;
+    }
+
+    public String getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(String unitId) {
+        this.unitId = unitId;
+    }
+
+    public String getUnitLength() {
+        return unitLength;
+    }
+
+    public void setUnitLength(String unitLength) {
+        this.unitLength = unitLength;
+    }
+
+    public String getExFromPosition() {
+        return exFromPosition;
+    }
+
+    public void setExFromPosition(String exFromPosition) {
+        this.exFromPosition = exFromPosition;
+    }
+
+    public String getExToPosition() {
+        return exToPosition;
+    }
+
+    public void setExToPosition(String exToPosition) {
+        this.exToPosition = exToPosition;
+    }
+
+    public Integer getWORKINGENDTIME() {
+        return WORKINGENDTIME;
+    }
+
+    public void setWORKINGENDTIME(Integer WORKINGENDTIME) {
+        this.WORKINGENDTIME = WORKINGENDTIME;
+    }
+}
